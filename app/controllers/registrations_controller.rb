432210b3986fc18065f85ebd970903bb4bff2d82
Chris C Cerami <chrisccerami@gmail.com>
2015-01-10 17:09:29 -0500

    Users can now only register for a workshop once

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def create
-    @registration = Registration.new(:workshop_id => params[:workshop_id])
+    @registration = Registration.find_or_initialize_by(:workshop_id => params[:workshop_id])
     @registration.user = current_user
     if @registration.save
       flash[:notice] = "Successfully registered!"
@@ -18,5 +18,4 @@ class RegistrationsController < ApplicationController
     flash[:notice]= "You are no longer registered for that class"
     redirect_to workshop_path(params[:workshop_id])
   end
-
 end
