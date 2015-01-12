a4b8e8503562d7bcee3fdfffe9b73a1f7ba69768
Cori Snedecor <coriannas@yahoo.com>
2015-01-11 22:37:25 -0500

    Gets rid of background image on workshop show page, fixes bug so every registration shows on workshop page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def create
-    @registration = Registration.find_or_initialize_by(:workshop_id => params[:workshop_id])
+    @registration = Registration.new(:workshop_id => params[:workshop_id])
     @registration.user = current_user
     if @registration.save
       flash[:notice] = "Successfully registered!"
